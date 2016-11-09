1702af5d2a470391fe39ad7e19e9bb9be9ed4b38
Liuhongliang <1029765111@qq.com>
2016-11-09 09:31:13 +0800

    增加baidu.js

---
+++ src/Jueci.MobileWeb.Web/Scripts/others/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ae89c43ea25b307c8211270602f182ae";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
